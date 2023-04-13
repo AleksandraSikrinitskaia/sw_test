17dea0f5281204b84694d8271005a305f4bdb342
Aleksandra_Sikrinitckaia <Aleksandra_Sikrinitckaia@epam.com>
2023-04-13 13:56:03 -0700

    testBudgetPagination testStatsSortOrder

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -2,7 +2,8 @@ package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object BudgetService {
@@ -19,22 +20,41 @@ object BudgetService {
         }
     }
 
-    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
-        transaction {
-            val query = BudgetTable
-                .select { BudgetTable.year eq param.year }
-                .limit(param.limit, param.offset)
+    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse =
+        withContext(Dispatchers.IO) {
+            transaction {
+                val pageQuery = BudgetTable
+                    .select { BudgetTable.year eq param.year }
+                    .orderBy(BudgetTable.month to SortOrder.ASC)
+                    .orderBy(BudgetTable.amount to SortOrder.DESC)
+                    .limit(param.limit, param.offset)
 
-            val total = query.count()
-            val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
+                val countColumn = BudgetTable.year.count()
+                val sumAmount = BudgetTable.amount.sum();
 
-            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
+                val statData = BudgetTable
+                    .slice(BudgetTable.type, countColumn, sumAmount)
+                    .select(BudgetTable.year eq param.year)
+                    .groupBy(BudgetTable.type)
+                    .toList()
 
-            return@transaction BudgetYearStatsResponse(
-                total = total,
-                totalByType = sumByType,
-                items = data
-            )
+                val total = statData.map { it[countColumn] }.sum()
+
+                val sumByType = statData
+                    .groupBy { it[BudgetTable.type].name }
+                    .mapValues { it.value.first()[sumAmount]!! }
+
+                val pageData = BudgetEntity.wrapRows(pageQuery).map { it.toResponse() }
+
+                return@transaction BudgetYearStatsResponse(
+                    total = total,
+                    totalByType = sumByType,
+                    items = pageData
+                )
+            }
+
         }
-    }
+
 }
+
+
